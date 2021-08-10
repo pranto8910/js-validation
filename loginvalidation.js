3d7dd29a01f371af13d454b441213c276b78b5c1
Kamal Pranto <pranto8910@gmail.com>
2021-08-10 13:17:30 +0600

    test

---
+++ loginvalidation.js
@@ -0,0 +1,22 @@
+
+    function jsValid() 
+    { 
+  
+        var username = document.forms["LoginForm"]["username"].value;
+        var password = document.forms["LoginForm"]["password"].value;
+ 
+ 
+        if (username === "" ) 
+        {
+            document.getElementById('UsernameErr').innerHTML = "Username can not be empty.";
+            return false;
+        } 
+
+        if (password === "" ) 
+        {
+            document.getElementById('PasswordErr').innerHTML = "password can not be empty.";
+            return false;
+        } 
+ 
+    }
+ 
